9daedc4c25bdedde6a467b7bdd483681346e938a
Zvi Baratz <z.baratz@gmail.com>
2019-01-23 14:38:49 +0200

    Improved BetConfiguration and BetRun models

--- django_nipype/models/interfaces/fsl/bet/bet_configuration.py
+++ django_nipype/models/interfaces/fsl/bet/bet_configuration.py
@@ -1,41 +1,26 @@
-from django.contrib.postgres.fields import ArrayField
 from django.core.validators import validate_comma_separated_integer_list
 from django.db import models
 from django.forms.models import model_to_dict
+from django_nipype.models.fields import ChoiceArrayField
 from django_nipype.models import NodeConfiguration
+from nipype.interfaces.fsl import BET
+from nipype.pipeline import Node
+
+
+def default_output():
+    return [BetConfiguration.BRAIN]
 
 
 class BetConfiguration(NodeConfiguration):
 
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+    # Skull stripping configuration
     CONFIG_DICT = {
-        "output_surface_outline": "outline",
-        "output_binary_mask": "mask",
-        "output_skull": "skull",
-        "output_mesh_surface": "mesh",
         "fractional_intensity_threshold": "frac",
         "head_radius": "radius",
         "threshold_segmented": "threshold",
     }
-
-    # Mode configuration choices
-    NORMAL = None
-    ROBUST = "ROBU"
-    PADDING = "PADD"
-    REMOVE_EYES = "REMO"
-    SURFACES = "SURF"
-    FUNCTIONAL = "FUNC"
-    REDUCE_BIAS = "REDU"
-    MODE_CHOICES = (
-        (NORMAL, "Normal"),
-        (ROBUST, "Robust"),
-        (PADDING, "Padding"),
-        (REMOVE_EYES, "Remove Eyes"),
-        (SURFACES, "Surfaces"),
-        (FUNCTIONAL, "Functional"),
-        (REDUCE_BIAS, "Reduce Bias"),
-    )
-
-    # Skull stripping configuration
     fractional_intensity_threshold = models.FloatField(
         default=0.5, help_text="Fractional intensity threshold"
     )
@@ -56,37 +41,91 @@ class BetConfiguration(NodeConfiguration):
     threshold_segmented = models.BooleanField(
         default=False, help_text="Apply thresholding to segmented brain image and mask"
     )
-    mode = models.CharField(max_length=4, choices=MODE_CHOICES, default=NORMAL)
+
+    # Mode configuration choices
+    NORMAL = None
+    ROBUST = "ROBU"
+    PADDING = "PADD"
+    REMOVE_EYES = "REMO"
+    SURFACES = "SURF"
+    FUNCTIONAL = "FUNC"
+    REDUCE_BIAS = "REDU"
+    MODE_CHOICES = (
+        (NORMAL, "Normal"),
+        (ROBUST, "Robust"),
+        (PADDING, "Padding"),
+        (REMOVE_EYES, "Remove Eyes"),
+        (SURFACES, "Surfaces"),
+        (FUNCTIONAL, "Functional"),
+        (REDUCE_BIAS, "Reduce Bias"),
+    )
+    mode = models.CharField(
+        max_length=4, choices=MODE_CHOICES, default=NORMAL, blank=True
+    )
 
     # Output files configuration
-
-    output_files = ArrayField(models.CharField(max_length=20), size=5)
-    output_surface_outline = models.BooleanField(
-        default=False, help_text="Create surface outline image"
+    BRAIN = "BRN"
+    SURFACE_OUTLINE = "SRF"
+    BINARY_MASK = "MSK"
+    SKULL = "SKL"
+    MESH_SURFACE = "MSH"
+    OUTPUT_CHOICES = (
+        (BRAIN, "Brain"),
+        (SURFACE_OUTLINE, "Surface Outline"),
+        (BINARY_MASK, "Binary Mask"),
+        (SURFACES, "Surfaces"),
+        (FUNCTIONAL, "Functional"),
     )
-    output_binary_mask = models.BooleanField(
-        default=False, help_text="Create binary mask image"
-    )
-    output_skull = models.BooleanField(default=False, help_text="Create skull image")
-    output_mesh_surface = models.BooleanField(
-        default=False, help_text="Generate a VTK mesh brain surface"
-    )
-    no_output = models.BooleanField(
-        default=False, help_text="Don't generate segmented output"
+    # Each possible output file has a corresponding trait in BET configuration
+    OUTPUT_TRAITS = {
+        BRAIN: "out_file",
+        SURFACE_OUTLINE: "outline",
+        BINARY_MASK: "mask",
+        SKULL: "skull",
+        MESH_SURFACE: "mesh",
+    }
+    output = ChoiceArrayField(
+        models.CharField(max_length=3, choices=OUTPUT_CHOICES),
+        size=5,
+        blank=True,
+        default=default_output,
     )
 
     class Meta:
         verbose_name_plural = "Configurations"
 
-    def create_kwargs(self):
+    def get_configuration_dict(self) -> dict:
         d = model_to_dict(self)
-        skip = ["id", "name", "mode"]
-        kwargs = {
+        skip = ["id", "name", "mode", "output"]
+        config = {
             self.CONFIG_DICT.get(key, key): value
             for key, value in d.items()
             if key not in skip and value
         }
+        return config
+
+    def add_mode_to_config(self, config: dict) -> dict:
         if self.mode:
-            trait_name = self.get_mode_display().lower()
-            kwargs[trait_name] = True
-        return kwargs
+            trait_name = self.get_mode_display().lower().replace(" ", "_")
+            config[trait_name] = True
+        return config
+
+    def add_output_to_config(self, config: dict) -> dict:
+        for output_file in self.output:
+            if output_file is not self.BRAIN:
+                trait_name = self.OUTPUT_TRAITS[output_file]
+                config[trait_name] = True
+        return config
+
+    def create_kwargs(self) -> dict:
+        config = self.get_configuration_dict()
+        config = self.add_mode_to_config(config)
+        config = self.add_output_to_config(config)
+        return config
+
+    def create_bet_instance(self) -> BET:
+        return BET(**self.create_kwargs())
+
+    def create_node(self) -> Node:
+        bet = self.create_bet_instance()
+        return Node(bet, name=self.name or "bet_node")
--- django_nipype/models/interfaces/fsl/bet/bet_run.py
+++ django_nipype/models/interfaces/fsl/bet/bet_run.py
@@ -3,17 +3,15 @@ import os
 from django.db import models
 from django_nipype.apps import DjangoNipypeConfig
 from django_nipype.models import NodeRun
-from nipype.interfaces.fsl import BET
 
 
-BET_DIR = os.path.join(DjangoNipypeConfig.RESULTS_PATH, "BET")
+BET_RESULTS = os.path.join(DjangoNipypeConfig.RESULTS_PATH, "BET")
 
 
 class BetRun(NodeRun):
     in_file = models.FilePathField(
         path=DjangoNipypeConfig.DB_PATH, max_length=255, match="*.nii*", recursive=True
     )
-
     configuration = models.ForeignKey(
         "django_nipype.BetConfiguration",
         on_delete=models.PROTECT,
@@ -23,14 +21,11 @@ class BetRun(NodeRun):
     class Meta:
         verbose_name_plural = "Runs"
 
-    def create_out_file_path(self):
-        file_name = f"{self.id}.nii.gz"
-        os.makedirs(BET_DIR, exist_ok=True)
-        return os.path.join(BET_DIR, file_name)
+    def default_out_path(self) -> str:
+        return os.path.join(BET_RESULTS, f"{self.id}.nii.gz")
 
     def run(self):
-        config = self.configuration.create_kwargs()
-        bet = BET(**config)
-        bet.inputs.in_file = self.in_file
-        bet.inputs.out_file = self.create_out_file_path()
-        bet.run()
+        node = self.configuration.create_node()
+        node.inputs.in_file = self.in_file
+        node.inputs.out_file = self.default_out_path()
+        node.run()
